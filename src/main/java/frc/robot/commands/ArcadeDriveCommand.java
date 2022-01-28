06e5ad110eb66f9570ea533424d5a697b272c50f
Daniel Freer <djfreer@icloud.com>
2022-01-28 17:36:18 -0500

    Added to ArcadeDriveCommand

--- src/main/java/frc/robot/commands/ArcadeDriveCommand.java
+++ src/main/java/frc/robot/commands/ArcadeDriveCommand.java
@@ -9,35 +9,36 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 
 /** An example command that uses an example subsystem. */
 public class ArcadeDriveCommand extends CommandBase {
+	private OI m_oi;
+	DriveTrain m_drivetrain;
 
+	/**
+	 * Creates a new ArcadeDriveCommand.
+	 *
+	 * @param subsystem The subsystem used by this command.
+	 */
+	public ArcadeDriveCommand() {
+		m_oi = OI.getInstance();
+		m_drivetrain = DriveTrain.getDriveTrain();
+		// Use addRequirements() here to declare subsystem dependencies.
+		//addRequirements();
+	}
 
+	// Called when the command is initially scheduled.
+	@Override
+	public void initialize() {}
 
-  /**
-   * Creates a new ArcadeDriveCommand.
-   *
-   * @param subsystem The subsystem used by this command.
-   */
-  public ArcadeDriveCommand() {
+	// Called every time the scheduler runs while the command is scheduled.
+	@Override
+	public void execute() {}
 
-    // Use addRequirements() here to declare subsystem dependencies.
-    //addRequirements();
-  }
+	// Called once the command ends or is interrupted.
+	@Override
+	public void end(boolean interrupted) {}
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {}
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {}
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
+	// Returns true when the command should end.
+	@Override
+	public boolean isFinished() {
+		return false;
+	}
 }
