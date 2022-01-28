ebf93b2e2bfe177c5ded4ddad15bc8fef537c399
Daniel Freer <djfreer@icloud.com>
2022-01-28 16:30:11 -0500

    Moved arm to climber

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -5,23 +5,22 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import frc.robot.Constants;
+import frc.robot.OI;
 
 public class Climber extends SubsystemBase {
-
+	private WPI_VictorSPX arm;
+	private WPI_VictorSPX intake;
 
 	//Constructor
 	public Climber() {
-
+		//Creates VictorSPX motor controller for the arm
+		arm=new WPI_VictorSPX(Constants.MCID.get("Arm"));
 	}
 
-	@Override
-	public void periodic() {
-		// This method will be called once per scheduler run
-	}
-
-	@Override
-	public void simulationPeriodic() {
-		// This method will be called once per scheduler run during simulation
+	//Returns the arm motor controller
+	public WPI_VictorSPX getMotor() {
+		return arm;
 	}
 }
