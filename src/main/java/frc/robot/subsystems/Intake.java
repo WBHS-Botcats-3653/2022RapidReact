d1e4977006b755ac3e05da54a69371534b48a104
Daniel Freer <djfreer@icloud.com>
2022-01-28 16:30:38 -0500

    Updated motor controller to wpi_victorspx

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,29 +1,27 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.MotorSafety;
-import edu.wpi.first.motorcontrol.PWMMotorController;
-import edu.wpi.first.motorcontrol.PWMVictorSPX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import frc.robot.Constants;
 import frc.robot.OI;
 
 public class Intake {
-	private VictorSPX intakePivot;
-	private VictorSPX rollers;
+	private WPI_VictorSPX intakePivot;
+	private WPI_VictorSPX rollers;
 
 	//Constructor
 	public Arm() {
 		//Creates VictorSPX motor controller for the arm
-		intakePivot=new VictorSPX(Constants.MCID.get("Intake Pivot"));
-		rollers=new VictorSPX(Constants.MCID.get("Rollers"));
+		intakePivot=new WPI_VictorSPX(Constants.MCID.get("Intake Pivot"));
+		rollers=new WPI_VictorSPX(Constants.MCID.get("Rollers"));
 	}
 
 	//Returns the pivot motor controller
-	public VictorSPX getPivotMotor() {
+	public WPI_VictorSPX getPivotMotor() {
 		return intakePivot;
 	}
 
-	public VictorSPX getRollerMotor() {
+	public WPI_VictorSPX getRollerMotor() {
 		return rollers;
 	}
 }
