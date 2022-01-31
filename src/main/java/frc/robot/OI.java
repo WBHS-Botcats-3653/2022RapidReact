6ceef43aea2209511274d6bdf48ba953ebaa5d16
Daniel Freer <djfreer@icloud.com>
2022-01-31 18:04:54 -0500

    nothing

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2021-2022 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -94,7 +94,7 @@ public class OI {
 
 		return ret_value * m_maxIntakeSpeed;
 	}
-
+	//Easter egg \o/
 	public boolean getIntakeDown() {
 		return m_controller.getLeftTriggerAxis() > 0 && !isIntakeDown;
 	}
