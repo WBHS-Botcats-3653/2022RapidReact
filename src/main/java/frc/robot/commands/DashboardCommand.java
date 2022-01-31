c4bd79746542bbb37c8f83687cbb3281b5d8cdca
Ruben Hadjes <rhadjesr@gmail.com>
2022-01-31 17:23:23 -0500

    created the dashboard
    We will be using the shuffleboard

---
+++ src/main/java/frc/robot/commands/DashboardCommand.java
@@ -0,0 +1,48 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+//Import CommandBase
+import edu.wpi.first.wpilibj2.command.CommandBase;
+//Import OI
+import frc.robot.OI;
+import frc.robot.subsystems.Dashboard;
+//Import Intake subsystem
+import frc.robot.subsystems.Intake;
+
+public class DashboardCommand extends CommandBase {
+	//Holds instances of OI and Intake subsystem
+	private OI m_oi;
+	private Dashboard m_dashboard;
+
+	/**
+	 * Creates a new DashboardCommand.
+	 *
+	 * @param subsystem The subsystem used by this command.
+	 */
+	public DashboardCommand() {
+		//Initializes instance variables with OI and Intake subsystem
+		m_oi=OI.getInstance();
+		m_dashboard= Dashboard.getDashboard();
+		// Use addRequirements() here to declare subsystem dependencies.
+		//addRequirements();
+	}
+
+	// Called when the command is initially scheduled.
+	public void initialize() {}
+
+	// Called every time the scheduler runs while the command is scheduled.
+	public void execute() {
+		
+	}
+
+	// Called once the command ends or is interrupted.
+	public void end(boolean interrupted) {}
+
+	// Returns true when the command should end.
+	public boolean isFinished() {
+		return false;
+	}
+}
---
+++ src/main/java/frc/robot/subsystems/Dashboard.java
@@ -0,0 +1,67 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+//Imports WPI_VictorSPX (MotorController)
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+//Imports SubsystemBase
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+//Imports Constants
+import frc.robot.Constants;
+
+public class Intake extends SubsystemBase {
+	private static Intake intake=null;
+	private WPI_VictorSPX pivot;
+	private WPI_VictorSPX rollers;
+	private WPI_VictorSPX belt;
+
+	/**Constructor
+	 * it is a singleton
+	 */
+	private Intake() {
+		//Creates VictorSPX motor controller for the arm
+		//Creates WPI_VictorSPX motor controller for the arm
+		pivot=new WPI_VictorSPX(Constants.MCID.get("Pivot"));
+		rollers=new WPI_VictorSPX(Constants.MCID.get("Rollers"));
+		belt=new WPI_VictorSPX(Constants.MCID.get("Belt"));
+	}
+
+	/**Returns an instance of the Intake
+	 * this helps with the singleton
+	 * 
+	 * @return
+	 */
+	public static Intake getIntake(){
+		if (intake == null)
+			intake = new Intake();
+		return intake;
+	}
+
+	//Spins the rollers on the intake
+	public void spinRollers(double speed) {
+		rollers.set(speed);
+	}
+
+	//Pivots the intake down
+	public void dropIntake() {
+		
+	}
+
+	//Pivots the intake up
+	public void raiseIntake() {
+
+	}
+
+	//Ejects cargo from intake
+	public void ejectCargo(double speed) {
+		rollers.set(-speed);
+	}
+
+	//Raises the cargo up the intake system
+	public void raiseCargo(double speed) {
+		belt.set(speed);
+	}
+}
