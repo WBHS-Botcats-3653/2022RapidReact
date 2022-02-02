29cbacb8b5f3f533f7a50b5132fd9052b0f29db4
Ruben Hadjes <rhadjesr@gmail.com>
2022-02-02 14:33:18 -0500

    Update AutoCommand.java

--- src/main/java/frc/robot/commands/AutoCommand.java
+++ src/main/java/frc/robot/commands/AutoCommand.java
@@ -2,11 +2,17 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.subsystems.DriveTrain;
-
+/**This command will run and will do 
+ * 
+ */
 public class AutoCommand extends CommandBase {
 	private DriveTrain driveTrain = DriveTrain.getDriveTrain();
 	//Constructor
+
+	
+
 	public AutoCommand() {
 		
 	}
@@ -14,7 +20,14 @@ public class AutoCommand extends CommandBase {
 	//@Override
 	public void initialize() {
 
-		new InstantCommand(() -> driveTrain.ArcadeDrived(0.5, 1.0), driveTrain);
+		/**what this will do is:
+		 * it will run the WaitUntilCommand (which will run for the time (in this case 3 seconds) and then stop)
+		 * because it is used the deadlineWith (which will make the parameter stop when the 
+		 * (in this case) WaitUntilCommand stops).
+		 * so it will basically go and .5 speed, straight, and it will last for 3 seconds, 
+		 * thus making the cut for the taxi drive part.
+		 */
+		new WaitUntilCommand(3).deadlineWith(new InstantCommand(() -> driveTrain.ArcadeDrived(0.5, 1.0), driveTrain));
 		// TODO Auto-generated method stub
 		//super.initialize();
 	}
