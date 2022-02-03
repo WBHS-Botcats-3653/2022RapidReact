6bedb01a1042a8abd31a67252cc4760f9f1420f8
Ruben Hadjes <rhadjesr@gmail.com>
2022-02-03 13:35:04 -0500

    added stuff

--- src/main/java/frc/robot/commands/ShooterCommand.java
+++ src/main/java/frc/robot/commands/ShooterCommand.java
@@ -4,16 +4,14 @@
 
 package frc.robot.commands;
 
-<<<<<<< HEAD
-import edu.wpi.first.wpilibj.command.InstantCommand;
 //Import CommandBase
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.RunCommand;
-=======
+
 //Import InstantCommand
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
->>>>>>> 6e7fcb9adaee3acbe950271b4fd4b8f72ea06ea2
+
 //Import OI
 import frc.robot.OI;
 import frc.robot.commands.subcommands.Indexersubcommands.IndexerCommand;
@@ -39,23 +37,23 @@ public class ShooterCommand extends ParallelCommandGroup {
 
 	// Called every time the scheduler runs while the command is scheduled.
 	public void execute() {
-<<<<<<< HEAD
+
 		//m_shooter.spinSpinner(m_oi.getShoot());
 		shoot = m_oi.getShoot();
-=======
->>>>>>> 6e7fcb9adaee3acbe950271b4fd4b8f72ea06ea2
+
+
 		addCommands(
 			new RunCommand(
 				() -> m_shooter.spinSpinner(shoot),
 					m_shooter
-<<<<<<< HEAD
+
 			)
 			.withInterrupt(() -> m_oi.getShoot() == 0),
-			new IndexerCommand().withInterrupt(() -> m_oi.getShoot() == 0)
-=======
-			),
 			new IndexerCommand()
->>>>>>> 6e7fcb9adaee3acbe950271b4fd4b8f72ea06ea2
+				.withInterrupt(
+					() -> m_oi.getShoot() == 0
+				)
+
 		);
 	}
 
--- src/main/java/frc/robot/commands/subcommands/Indexersubcommands/IndexerCommand.java
+++ src/main/java/frc/robot/commands/subcommands/Indexersubcommands/IndexerCommand.java
@@ -8,7 +8,6 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Storage;
 
 public class IndexerCommand extends CommandBase {
-<<<<<<< HEAD
   /** Creates a new IndexerCommand. */
   private Storage m_indexer;
   public IndexerCommand() {
@@ -22,35 +21,18 @@ public class IndexerCommand extends CommandBase {
     m_indexer.raiseCargo(1.0);
     
   }
-=======
-	/** Creates a new IndexerCommand. */
-	public IndexerCommand() {
-		// Use addRequirements() here to declare subsystem dependencies.
-	}
 
-	// Called when the command is initially scheduled.
-	@Override
-	public void initialize() {
-
-		
-	}
->>>>>>> 6e7fcb9adaee3acbe950271b4fd4b8f72ea06ea2
 
 	// Called every time the scheduler runs while the command is scheduled.
 	@Override
 	public void execute() {}
 
-<<<<<<< HEAD
+
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
     m_indexer.raiseCargo(0);
-=======
-	// Called once the command ends or is interrupted.
-	@Override
-	public void end(boolean interrupted) {
 
->>>>>>> 6e7fcb9adaee3acbe950271b4fd4b8f72ea06ea2
 
 	}
 
