6438a0c65e5a95cde27fd7197e8e0228d552b6d9
Daniel Freer <djfreer@icloud.com>
2022-02-04 18:23:00 -0500

    made singleton in shooterlogic

--- src/main/java/frc/robot/subsystems/logic/ShooterLogic.java
+++ src/main/java/frc/robot/subsystems/logic/ShooterLogic.java
@@ -1,5 +1,16 @@
 package frc.robot.subsystems.logic;
 
 public class ShooterLogic {
-	
+	public static ShooterLogic m_shooterLogic = null;
+
+	public ShooterLogic() {
+		
+	}
+
+	public static ShooterLogic getInstance() {
+		if (m_shooterLogic == null) {
+			m_shooterLogic = new ShooterLogic();
+		}
+		return m_shooterLogic;
+	}
 }
