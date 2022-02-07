11d60d0fd401ac24a667092e22452d83acf139eb
Ruben Hadjes <rhadjesr@gmail.com>
2022-02-07 16:40:10 -0500

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -176,6 +176,6 @@ public class Robot extends TimedRobot {
 	@Override
 	public void testPeriodic() {
 
-		new PrintCommand("testing the button:" + m_si.getPivotUpTriggered()).initialize();
+		//new PrintCommand("testing the button:" + m_si.getPivotUpTriggered()).initialize();
 	}
 }
