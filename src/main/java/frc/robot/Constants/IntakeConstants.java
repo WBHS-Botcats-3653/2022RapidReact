bc7971249d38b3ad94bd8741b89e98fe2202d46b
Daniel Freer <djfreer@icloud.com>
2022-02-12 12:55:11 -0500

    fixed ids

--- src/main/java/frc/robot/Constants/IntakeConstants.java
+++ src/main/java/frc/robot/Constants/IntakeConstants.java
@@ -6,8 +6,8 @@ public class IntakeConstants {
 	public static final int rollersMotorID = 21;
 	public static final int indexerMotorID = 22;
 	//Storeage photo electric sensor IDs
-	public static final int lowerStoragePESensorID = 2;
-	public static final int upperStoragePESensorID = 3;
+	public static final int lowerStoragePESensorID = 8;
+	public static final int upperStoragePESensorID = 9;
 	//Pivot switch IDs
 	public static final int pivotDownButtonID = 0;
 	public static final int pivotUpButtonID = 1;
--- src/main/java/frc/robot/Constants/ShooterConstants.java
+++ src/main/java/frc/robot/Constants/ShooterConstants.java
@@ -4,5 +4,5 @@ public class ShooterConstants {
 	//Spinner motor ID
 	public static final int spinnerMotorID = 30;
 	//Shooter photo electric sensor ID
-	public static final int shooterPESensorID = 4;
+	public static final int shooterPESensorID = 7;
 }
