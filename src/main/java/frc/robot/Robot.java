873a37b600324c93e2de3581683fbb7cd34b16bc
christiandurant <djfreer@icloud.com>
2022-02-20 10:27:24 -0500

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -141,8 +141,8 @@ public class Robot extends TimedRobot {
 		Dashboard.UpdateEncoderForTest(m_directionSubsystem.getLeftDistance());
 		//Runs the scheduler
 		Scheduler.getInstance().run();
-		//new PrintCommand("Distance left " + m_directionSubsystem.getLeftDistance()).initialize();
-		//new PrintCommand("Distance right " + m_directionSubsystem.getRightDistance()).initialize();
+		new PrintCommand("Distance left " + m_directionSubsystem.getLeftDistance()).initialize();
+		new PrintCommand("Distance right " + m_directionSubsystem.getRightDistance()).initialize();
 		new PrintCommand("Shooter PE: "+m_si.getShooterTriggered()).initialize();
 		new PrintCommand("Upper Storage PE: "+m_si.getUpperStorageTriggered()).initialize();
 		new PrintCommand("Lower Storage PE: "+m_si.getLowerStorageTriggered()).initialize();
