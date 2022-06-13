086630bc85cc1b55aabe586dddbb523e7205f18f
Daniel Freer <djfreer@icloud.com>
2022-06-12 23:47:11 -0400

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -54,10 +54,6 @@ public class Robot extends TimedRobot {
 		// Instantiate our RobotContainer.  This will perform all our button bindings, and put our
 		// autonomous chooser on the dashboard.
 		m_robotContainer = new RobotContainer();
-		//Calibrates the gyro
-		m_direction.calibrateGyro();
-		//Resets the encoders
-		m_direction.resetEncoders();
 	}
 
 	/**
